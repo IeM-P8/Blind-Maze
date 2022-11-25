9da374599fc0d24cf1cdf0699df96501ed9f6ef0
Roucool_PvP <lucastag77@gmail.com>
2022-11-25 13:46:48 +0100

    Renommage fenêtre

--- source/game_manager.py
+++ source/game_manager.py
@@ -17,7 +17,7 @@ class GameManager():
     def __init__(self):
         #Initialisation
         pygame.init()
-        pygame.display.set_caption("Pygame Window")
+        pygame.display.set_caption("Blind Maze")
         self.fen = pygame.display.set_mode(const.RESOLUTION, pl.FULLSCREEN)
         pygame.key.set_repeat(500, 500)
         self._is_started = False
