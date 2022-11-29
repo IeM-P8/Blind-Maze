d50a17571fc011831b2041142b3b9412021dd968
Roucool_PvP <lucastag77@gmail.com>
2022-11-29 11:17:36 +0100

    Tentative 2

--- source/game_manager.py
+++ source/game_manager.py
@@ -17,7 +17,7 @@ class GameManager():
         #Initialisation
         pygame.init()
         pygame.display.set_caption("Blind Maze")
-        self.fen = pygame.display.set_mode(const.RESOLUTION, pl.FULLSCREEN)
+        self.fen = pygame.display.set_mode(const.RESOLUTION, pl.FULLSCREEN | pl.HWSURFACE | pl.DOUBLEBUF)
         pygame.key.set_repeat(500, 500)
         self._is_started = False
 
